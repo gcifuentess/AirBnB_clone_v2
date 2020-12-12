1df579fa235639c607a1e5f6c85e0ed873dfa3e9
Athesto <gamez.live@gmail.com>
2020-12-12 14:10:21 -0500

    ignore users.noreply

--- hack/generate-authors.sh
+++ hack/generate-authors.sh
@@ -11,5 +11,5 @@ cd "$(dirname "$(readlink -f "$BASH_SOURCE")")/.."
 		# For how it is generated, see `hack/generate-authors.sh`.
 	EOH
 	echo
-	git log --format='%aN <%aE>' | LC_ALL=C.UTF-8 sort -uf
+	git log --format='%aN <%aE>' | LC_ALL=C.UTF-8 sort -uf | grep -v "users.noreply.github.com"
 } > AUTHORS
