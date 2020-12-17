cc1c6a01f2825264824ffb947f3ecc441dd77560
Gabriel Cifuentes <1795@holbertonschool.com>
2020-12-17 23:38:23 +0000

    Testing BaseModel classes without Amenity; pep8 checked

--- models/place.py
+++ models/place.py
@@ -3,8 +3,7 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String, ForeignKey, Float
 from os import getenv
-from models import storage, City
-from models import models
+from models.city import City
 
 
 class Place(BaseModel, Base):
@@ -13,7 +12,7 @@ class Place(BaseModel, Base):
     city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
     user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
     name = Column(String(128), nullable=False)
-    description = Column(String(1024), nullable=False)
+    description = Column(String(1024))
     number_rooms = Column(Integer, nullable=False)
     number_rooms = Column(Integer, nullable=False, default=0)
     number_bathrooms = Column(Integer, nullable=False, default=0)
--- models/state.py
+++ models/state.py
@@ -23,4 +23,3 @@ class State(BaseModel, Base):
                 if city.state_id == self.id:
                     list_cities.append(city)
             return list_cities
-
