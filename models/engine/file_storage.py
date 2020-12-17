20443495d8cd6c482e75fab4727e94df1bb671d6
dlscoccia <2054@holbertonschool.com>
2020-12-17 00:55:49 +0000

    Fixing if condition file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -10,7 +10,13 @@ class FileStorage:
 
     def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        if cls is not None:
+            new_dict = {}
+            for key, value in self.__objects.items():
+                if cls == value.__class__ or cls == value.__class__.__name__:
+                    new_dict[key] = value
+                return new_dict
+            return self.__objects
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
