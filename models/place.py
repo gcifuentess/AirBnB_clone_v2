d14cb4eac210463449936cb6ac89227959c0c28e
Athesto <gamez.live@gmail.com>
2020-12-17 22:54:42 -0500

    set nulleable implicitly

--- models/place.py
+++ models/place.py
@@ -12,14 +12,13 @@ class Place(BaseModel, Base):
     city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
     user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
     name = Column(String(128), nullable=False)
-    description = Column(String(1024), nulleable=True)
-    number_rooms = Column(Integer, nullable=True)
-    number_rooms = Column(Integer, nullable=True, default=0)
+    description = Column(String(1024))
+    number_rooms = Column(Integer, nullable=False, default=0)
     number_bathrooms = Column(Integer, nullable=False, default=0)
     max_guest = Column(Integer, nullable=False, default=0)
     price_by_night = Column(Integer, nullable=False, default=0)
-    latitude = Column(Float, nullable=True)
-    longitude = Column(Float, nullable=True)
+    latitude = Column(Float)
+    longitude = Column(Float)
     if getenv("HBNB_TYPE_STORAGE") is "file":
         @property
         def cities(self):
