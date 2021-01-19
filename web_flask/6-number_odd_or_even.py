2b3e450df340c4396c2324a5185dd2f456998471
Gabriel Cifuentes <1795@holbertonschool.com>
2021-01-19 22:52:48 +0000

    add web_flask/6-number_odd_or_even.py web_flask/templates/6-number_odd_or_even.html

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+'''Working with Flask Module'''
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_HBNB():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def HBNB():
+    return 'HBNB'
+
+
+@app.route('/c/<string:text>', strict_slashes=False)
+def show_post(text):
+    return "C {}".format(text.replace('_', ' '))
+
+
+@app.route('/python', defaults={"text": "is cool"},
+           strict_slashes=False)
+@app.route('/python/<string:text>', strict_slashes=False)
+def show_post_python(text):
+    return "Python {}".format(text.replace('_', ' '))
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    from flask import render_template
+    return render_template('5-number.html', number=n)
+
+
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def number_odd_or_even(n):
+    from flask import render_template
+    return render_template('6-number_odd_or_even.html', number=n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
