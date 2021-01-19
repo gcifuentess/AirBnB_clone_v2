a5493a2d55c6765340114db2c5ff1bb0e7dbd04f
Gabriel Cifuentes <1795@holbertonschool.com>
2021-01-19 22:11:27 +0000

    add ./web_flask/3-python_route.py

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+'''Working with Flask Module'''
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_HBNB():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def HBNB():
+    return 'HBNB'
+
+
+@app.route('/c/<string:text>', strict_slashes=False)
+def show_post(text):
+    return "C {}".format(text.replace('_', ' '))
+
+
+@app.route('/python', defaults={"text": "is cool"},
+           strict_slashes=False)
+@app.route('/python/<string:text>', strict_slashes=False)
+def show_post_python(text):
+    return "Python {}".format(text.replace('_', ' '))
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
