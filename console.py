30086164cd6e65b5db74923767486a8839d3339e
Gabriel Cifuentes <1795@holbertonschool.com>
2021-01-20 22:19:45 +0000

    modified:   console.py modified:   models/amenity.py modified:   models/engine/db_storage.py new file:   web_flask/10-hbnb_filters.py new file:   web_flask/static/images/icon.ico new file:   web_flask/static/images/icon_bath.png new file:   web_flask/static/images/icon_bed.png new file:   web_flask/static/images/icon_group.png new file:   web_flask/static/images/icon_pets.png new file:   web_flask/static/images/icon_tv.png new file:   web_flask/static/images/icon_wifi.png new file:   web_flask/static/images/logo.png new file:   web_flask/static/styles/100-places.css new file:   web_flask/static/styles/101-places.css new file:   web_flask/static/styles/102-common.css new file:   web_flask/static/styles/102-filters.css new file:   web_flask/static/styles/102-footer.css new file:   web_flask/static/styles/102-header.css new file:   web_flask/static/styles/102-places.css new file:   web_flask/static/styles/103-common.css new file:   web_flask/static/styles/103-filters.css new file:   web_flask/static/styles/103-footer.css new file:   web_flask/static/styles/103-header.css new file:   web_flask/static/styles/103-places.css new file:   web_flask/static/styles/2-common.css new file:   web_flask/static/styles/2-footer.css new file:   web_flask/static/styles/2-header.css new file:   web_flask/static/styles/3-common.css new file:   web_flask/static/styles/3-footer.css new file:   web_flask/static/styles/3-header.css new file:   web_flask/static/styles/4-common.css new file:   web_flask/static/styles/4-filters.css new file:   web_flask/static/styles/5-filters.css new file:   web_flask/static/styles/6-filters.css new file:   web_flask/static/styles/6-filters.css~ new file:   web_flask/static/styles/7-places.css new file:   web_flask/static/styles/8-places.css new file:   web_flask/templates/10-hbnb_filters.html

--- console.py
+++ console.py
@@ -168,7 +168,7 @@ class HBNBCommand(cmd.Cmd):
 
         key = c_name + "." + c_id
         try:
-            print(storage._FileStorage__objects[key])
+            print(storage.all()[key])
         except KeyError:
             print("** no instance found **")
 
@@ -200,7 +200,7 @@ class HBNBCommand(cmd.Cmd):
         key = c_name + "." + c_id
 
         try:
-            del(storage.all()[key])
+            storage.delete(storage.all()[key])
             storage.save()
         except KeyError:
             print("** no instance found **")
